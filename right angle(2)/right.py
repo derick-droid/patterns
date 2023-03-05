7258095749c6478d435daa79ca20dc8678e00bd0
derick-droid <derrickokinda9@gmail.com>
2023-03-05 16:41:42 +0300

    right angle pattern two

---
+++ right angle(2)/right.py
@@ -0,0 +1,9 @@
+def right():
+    row = int(input("Enter number of row: "))
+    for i in range(1, row + 1):
+        for j in range(1, i + 1):
+            print("*", end=" ")
+        print()
+
+
+right()
