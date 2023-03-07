a9fcc24cda8e6d6e726e06aa10c366a9b4fed58a
derick-droid <derrickokinda9@gmail.com>
2023-03-07 16:58:48 +0300

    complete trapezium and pyramid

--- trapezium/trapezium.py
+++ trapezium/trapezium.py
@@ -8,7 +8,7 @@ def pyramid():
     for i in range(0, row):
         for j in range(0, row - i - 1):
             print(end=" ")
-        for j in range(0, i + 1):
+        for j in range(0, row + 1):
             print("*", end=" ")
         print()
 
