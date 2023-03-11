588f585ae77707b6699c0cf4204d9bea647a4b54
derick-droid <derrickokinda9@gmail.com>
2023-03-11 20:49:10 +0300

    advancing in A program

--- A/A.py
+++ A/A.py
@@ -7,7 +7,7 @@ def a_shape():
     num = int(input("Enter the row: "))
     for row in range(0, num + 1):
         for col in range(0, num + 1):
-            if row != 0 or (col == 0 or col == 4) or (col == 4 and col == 0):
+            if row != 0 or (col == 0 or col == 4) and  (col == 4 and col == 0):
                 print("", end=" ")
             else:
                 print("*", end=" ")
