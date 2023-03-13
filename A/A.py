99ca8609f84c5c737b56e636a58fd80a981341c8
derick-droid <derrickokinda9@gmail.com>
2023-03-13 15:43:38 +0300

    complete A shape pattern

--- A/A.py
+++ A/A.py
@@ -7,10 +7,10 @@ def a_shape():
     num = int(input("Enter the row: "))
     for row in range(0, num + 1):
         for col in range(0, num + 1):
-            if row != 0 or (col == 0 or col == 4) and  (col == 4 and col == 0):
-                print("", end=" ")
-            else:
+            if ((col == 0 or col == 4) and row != 0) or ((row == 0 or row == 3) and (col > 0 and col<4)):
                 print("*", end=" ")
+            else:
+                print(end=" ")
         print()
 
 
