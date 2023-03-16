bdfd7be6b61ff4ed80e901389a2ed2c490b185f7
derick-droid <derrickokinda9@gmail.com>
2023-03-16 21:35:39 +0300

    complete pattern 2

--- number(2)/number2.py
+++ number(2)/number2.py
@@ -6,8 +6,9 @@ another number shape
 def number():
     num = int(input("Enter number: "))
     for i in range(0, num):
-        for j in range(0, num):
+        for j in range(i, -1, -1):
             print(j + 1, end=" ")
+        print()
 
 
 number()
