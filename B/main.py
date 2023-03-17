c47c1fcd6a9b35ab576d2009037092bbb3d96c21
derick-droid <derrickokinda9@gmail.com>
2023-03-17 14:43:03 +0300

    B shape in making

---
+++ B/main.py
@@ -0,0 +1,19 @@
+"""
+B shape
+"""
+
+
+def b_shape():
+    num = int(input("Enter row: "))
+    for row in range(num):
+        for col in range(num):
+            if (col == 0 or col == 4) or (row == 0 or row == 3 or row == 6) and (col > 0 and col < 4):
+                print("*", end=" ")
+            else:
+                print(end=" ")
+        print()
+
+
+b_shape()
+
+
