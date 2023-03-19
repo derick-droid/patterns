f190d352c50e90dd746166dbd65d926e2182c326
derick-droid <derrickokinda9@gmail.com>
2023-03-19 22:09:35 +0300

    python pattern

--- B(2)/b.py
+++ B(2)/b.py
@@ -2,5 +2,17 @@
 block B shape
 """
 
+
 def b_shape():
-    
+    num = int(input("Enter the row: "))
+    for row in range(num):
+        for col in range(num):
+            if (col == 0 and col < 6  and row == 0 or row == 3 or row == 6):
+                print("*", end=" ")
+
+        print()
+
+b_shape()
+
+
+
--- B/b.py
+++ B/b.py
@@ -4,10 +4,10 @@ B shape
 
 
 def b_shape():
-    num = int(input("Enter row: "))
-    for row in range(num):
-        for col in range(num - 1):
-            if (col == 0 or col == 4) or (row == 0 or row == 3 or row == 6) and (col > 0 and col < 4):
+    # num = int(input("Enter row: "))
+    for row in range(7):
+        for col in range(5):
+            if (col == 0 or col == 4) or ((row == 0 or row == 3 or row == 6) and (col > 0 and col < 4)):
                 print("*", end=" ")
             else:
                 print(end=" ")
